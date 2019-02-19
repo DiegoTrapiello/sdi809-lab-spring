9d353e6600939b6c420e7a2fdd82ebd4a085c6ef
DiegoTrapiello <diego_trapiello@hotmail.com>
2019-02-19 18:04:51 +0100

    “SDI-SpringBoot-Sesión-4.4-Consulta”

--- sdi809-lab-spring/src/main/java/com/uniovi/controllers/MarksController.java
+++ sdi809-lab-spring/src/main/java/com/uniovi/controllers/MarksController.java
@@ -1,5 +1,6 @@
 package com.uniovi.controllers;
 
+import java.security.Principal;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.uniovi.entities.Mark;
+import com.uniovi.entities.User;
 import com.uniovi.services.MarksService;
 import com.uniovi.services.UsersService;
 
@@ -32,8 +34,10 @@ public class MarksController {
 	private UsersService usersService;
 
 	@RequestMapping("/mark/list")
-	public String getList(Model model) {
-		model.addAttribute("markList", marksService.getMarks());
+	public String getList(Model model, Principal principal) {
+		String dni = principal.getName(); // DNI es el name de la autenticación
+		User user = usersService.getUserByDni(dni);
+		model.addAttribute("markList", marksService.getMarksForUser(user));
 		return "mark/list";
 	}
 
@@ -79,12 +83,15 @@ public class MarksController {
 	}
 
 	@RequestMapping("/mark/list/update")
-	public String updateList(Model model) {
-		model.addAttribute("markList", marksService.getMarks());
+	public String updateList(Model model, Principal principal) {
+		String dni = principal.getName(); // DNI es el name de la autenticación
+		User user = usersService.getUserByDni(dni);
+		model.addAttribute("markList", marksService.getMarksForUser(user));
 		return "mark/list :: tableMarks";
 	}
 
 	@RequestMapping(value = "/mark/{id}/resend", method = RequestMethod.GET)
+
 	public String setResendTrue(Model model, @PathVariable Long id) {
 		marksService.setMarkResend(true, id);
 		return "redirect:/mark/list";
